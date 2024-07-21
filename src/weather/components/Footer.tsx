43d3748401b8608cc574efe67701504bea3775a7
Jin Ding <yunjianding@gmail.com>
2024-07-21 14:59:17 -0400

    Remove extra whitespace in footer

--- src/weather/components/Footer.tsx
+++ src/weather/components/Footer.tsx
@@ -1,8 +1,8 @@
 import { Box, Typography } from "@mui/material";
 
 export const Footer = () => (
-  <Box sx={{ background: "#eee", pt: 3, pb: 2, px: 2, marginTop: "auto" }}>
-    <Box sx={{ mx: "auto", width: 1280 }}>
+  <Box sx={{ background: "#eee", pt: 3, pb: 2, marginTop: "auto" }}>
+    <Box sx={{ mx: "auto", width: 1280, px: 2 }}>
       <Typography>Weather App</Typography>
       <Typography color="grey" variant="body2">
         By Jin Ding
